bfded807f392242c15470437fba3ce015bc2fbb1
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-19 13:25:16 +0300

    Добавьте файлы проекта.

---
+++ Lab16_1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace Lab16_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] jsonProduct = new string[5];
+            string path = "Products.json";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = false
+            };
+
+            Product[] product = new Product[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите код товара: ");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите название товара: ");
+                string name = Console.ReadLine();
+                Console.Write("Введите цену товара: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                product[i] = new Product(code, name, price);
+                jsonProduct[i] = JsonSerializer.Serialize(product[i], options);
+            }
+
+            foreach (string j in jsonProduct)
+            {
+                Console.WriteLine(j);
+            }
+            
+            File.WriteAllLines(path, jsonProduct);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
+
+class Product
+{
+    [JsonPropertyName("code")]
+    public int Code { set; get; }
+    [JsonPropertyName("name")]
+    public string Name { set; get; }
+    [JsonPropertyName("price")]
+    public double Price { set; get; }
+
+    public Product(int code, string name, double price)
+    {
+        Code = code;
+        Name = name;
+        Price = price;
+    }
+}
+
---
+++ Lab16_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxPrice = 0;
+            string[] readText = new string[5];
+            string path = "C:/Users/v.medzakovskiy.SPB/source/repos/Lab16/Lab16_1/bin/Debug/Products.json";
+            Product[] product = new Product[5];
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    readText[i] = sr.ReadLine();
+                    product[i] = JsonSerializer.Deserialize<Product>(readText[i]);
+                    product[i].ShowInfo();
+                }
+            }
+                for (int i = 1; i < 5; i++)
+                {
+                    if (product[i].Price > product[maxPrice].Price)
+                    {
+                        maxPrice = i;
+                    }
+                }
+            Console.WriteLine($"Самый дорогой товар - {product[maxPrice].Name}, его цена: {product[maxPrice].Price}");
+            Console.ReadKey();
+        }
+    }
+
+
+    class Product
+    {
+        [JsonPropertyName("code")]
+        public int Code { set; get; }
+        [JsonPropertyName("name")]
+        public string Name { set; get; }
+        [JsonPropertyName("price")]
+        public double Price { set; get; }
+
+        public Product(int code, string name, double price)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Code + " " + Name + " " + Price);
+        }
+    }
+}
